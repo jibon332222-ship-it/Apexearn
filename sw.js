6a49ece1733fa9b4f3128af60470e2e726504c4f
jibon332222-ship-it <jibon332222@gmail.com>
2026-07-27 09:49:29 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  console.log('Service Worker Activated');
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
